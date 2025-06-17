960ee9b97307b260def146239a1e09b847cbbc02
Kanell13 <antoniskanellakis13@gmail.com>
2025-06-17 15:50:03 +0300

    Fix asset loading and deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   },
   server: {
     port: 3000
-  }
+  },
+  publicDir: 'assets'
 }) 
